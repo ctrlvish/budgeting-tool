801c8424c34c9655d96a032f57a2890508770ffc
ctrlvish <invishibl@gmail.com>
2026-06-27 07:39:16 +1000

    more settings page stuff

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -2,27 +2,86 @@ import { useEffect, useState } from "react";
 import type { BudgetSetting } from '../types'
 import { useBudgetSettings } from "../hooks/useBudgetSettings";
 
+const defaultSettings: BudgetSetting = {
+    monthlyIncome: 0,
+    startingSavingsBalance: 0,
+    needs: 50,
+    wants: 30,
+    savings: 20
+}
+
 export default function Settings(){
     const { settings, save } = useBudgetSettings()
 
+    const [formData, setFormData] = useState<BudgetSetting>(defaultSettings)
+
+    const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target
+
+        setFormData((prevData) => ({
+            ...prevData,
+            [name] : Number(value)
+        }))
+
+    }
+
+    const handleSubmit = (e : React.SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        save(formData)
+    }
+
+    useEffect(() => {
+        settings && setFormData(settings)
+    }, [settings])
+
     return (
-    <>
-    <label htmlFor='monthlyIncomeInput'>Monthly Income:</label>
-    <input type = 'text' id="monthlyIncomeInput"></input>
-    
-    <label htmlFor='startingSavingsInput'>Starting savings balance:</label>
-    <input type = 'text' id="startingSavingsInput"></input>
+    <form onSubmit={handleSubmit}>
+        <label htmlFor='monthlyIncomeInput'>Monthly Income:</label>
+        <input 
+            type = 'number' 
+            id="monthlyIncomeInput"
+            name="monthlyIncome"
+            value={formData.monthlyIncome}
+            onChange={handleChange}
+        ></input>
+        
+        <label htmlFor='startingSavingsBalanceInput'>Starting savings balance:</label>
+        <input 
+            type = 'number' 
+            id="startingSavingsBalanceInput"
+            name="startingSavingsBalance"
+            value={formData.startingSavingsBalance}
+            onChange={handleChange}
+        ></input>
 
-    <label htmlFor='needsInput'>Needs:</label>
-    <input type = 'text' id="needsInput"></input>
+        <label htmlFor='needsInput'>Needs:</label>
+        <input 
+            type = 'number' 
+            id="needsInput"
+            name="needs"
+            value={formData.needs}
+            onChange={handleChange}
+        ></input>
 
-    <label htmlFor='wantsInput'>Wants:</label>
-    <input type = 'text' id="wantsInput"></input>
+        <label htmlFor='wantsInput'>Wants:</label>
+        <input 
+            type = 'number' 
+            id="wantsInput"
+            name="wants"
+            value={formData.wants}
+            onChange={handleChange}
+        ></input>
 
-    <label htmlFor='savingsInput'>Savings:</label>
-    <input type = 'text' id="savingsInput"></input>
+        <label htmlFor='savingsInput'>Savings:</label>
+        <input 
+            type = 'number' 
+            id="savingsInput"
+            name="savings"
+            value={formData.savings}
+            onChange={handleChange}
+        ></input>
 
-    <button>save</button>
-    </>
+        <button type="submit">save</button>
+    </form>
     )
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -8,11 +8,11 @@ export interface Category {
 }
 
 export interface BudgetSetting {
-    monthlyIncome : number | null
-    startingSavingsBalance : number | null
-    needs : number | null //ratios
-    wants : number | null
-    savings : number | null
+    monthlyIncome : number
+    startingSavingsBalance : number
+    needs : number //ratios
+    wants : number
+    savings : number
 }
 
 export interface RecurringExpense {
