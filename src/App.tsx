7baf5657e22d5c938962d0537e145ee5a40daa87
ctrlvish <invishibl@gmail.com>
2026-07-19 17:23:31 +1000

    feat: app header and placeholder pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import Settings from './pages/Settings'
+import AppHeader from './components/AppHeader'
 
 function App() {
-  return (<Settings/>)
+  return (<><AppHeader/><Settings/></>)
 }
 
 export default App
---
+++ src/components/AppHeader.tsx
@@ -0,0 +1,20 @@
+import { Button } from "./ui/button"
+
+export default function AppHeader(){
+    return (
+        <header className="grid max-w-4xl mx-auto grid-cols-[1fr_auto_1fr] border-b py-2 px-5">
+            <div className="">
+                <p>budgeting tool</p>
+            </div>
+            <nav className="flex gap-3">
+                <Button variant='ghost'>home</Button>
+                <Button variant='ghost'>transactions</Button>
+                <Button variant='secondary'>settings</Button>
+            </nav>
+            <div className="flex gap-2 justify-self-end">
+                <Button type='button' variant='ghost' aria-label="Toggle Theme">🌙</Button>
+                <Button type='button' aria-label="Add Transaction">+</Button>
+            </div>
+        </header>
+    )
+}
