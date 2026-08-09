9eebd4edc3ce186b4d4f226dcffd44afd98ff8fb
ctrlvish <invishibl@gmail.com>
2026-08-09 12:18:28 +1000

    chore: ignore dists

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'dev-dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -25,7 +25,7 @@ export default defineConfig([
       'src/components/theme-provider.tsx',
     ],
     rules: {
-        'react-refresh/only-export-components': 'off'
-    }
-  }
+      'react-refresh/only-export-components': 'off',
+    },
+  },
 ])
